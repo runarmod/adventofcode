cfa220e6dc3540a16acbc416d8858c414584bdb5
Runar Saur Modahl <runarmod@gmail.com>
2022-07-29 22:06:18 +0200

    2015 12

---
+++ 2015/12/main.py
@@ -0,0 +1,41 @@
+import re
+from json import loads
+
+
+class Solution():
+    def __init__(self, test=False):
+        self.test = test
+        filename = "testinput.txt" if self.test else "input.txt"
+        self.data = open(filename).read().rstrip()
+        self.jsn = loads(self.data)
+
+    def n(self, j):
+        if type(j) == int:
+            return j
+        if type(j) == list:
+            return sum(self.n(i) for i in j)
+        if type(j) != dict:
+            return 0
+        if "red" in j.values():
+            return 0
+        return self.n(list(j.values()))
+
+    def part1(self):
+        return sum(map(int, re.findall(r"(-?\d+)", self.data)))
+
+    def part2(self):
+        return self.n(self.jsn)
+
+
+def main():
+    solution = Solution()
+    print(part1 := f"Part 1: {solution.part1()}")
+    print(part2 := f"Part 2: {solution.part2()}")
+
+    if not solution.test:
+        with open("solution.txt", "w") as f:
+            f.write(f"{part1}\n{part2}\n")
+
+
+if __name__ == "__main__":
+    main()
