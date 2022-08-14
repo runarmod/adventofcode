9311da86c66f1348c5b5755b42bf45cc4ec89335
Runar Saur Modahl <runarmod@gmail.com>
2022-08-14 02:05:15 +0200

    2015 25
    have to get last 3 stars to complete the whole calender

---
+++ 2015/25/main.py
@@ -0,0 +1,37 @@
+class Solution:
+    def __init__(self, test=False):
+        self.test = test
+        self.row = 2981
+        self.col = 3075
+
+    def part1(self):
+        val = 20151125
+        x = y = maxx = 1
+        for _ in range(1000000000000):
+            if y <= 1:
+                maxx += 1
+                y = maxx
+                x = 1
+            else:
+                x += 1
+                y -= 1
+            val = (val * 252533) % 33554393
+            if y == self.row and x == self.col:
+                return val
+
+    def part2(self):
+        return None
+
+
+def main():
+    solution = Solution()
+    print(part1 := f"Part 1: {solution.part1()}")
+    print(part2 := f"Part 2: {solution.part2()}")
+
+    if not solution.test:
+        with open("solution.txt", "w") as f:
+            f.write(f"{part1}\n{part2}\n")
+
+
+if __name__ == "__main__":
+    main()
