730caf2ed164bf7b6fcf482deaed48be9388eeee
Runar Saur Modahl <runarmod@gmail.com>
2022-08-17 01:21:15 +0200

    2016 9

---
+++ 2016/09/main.py
@@ -0,0 +1,39 @@
+import re
+
+
+class Solution:
+    def __init__(self, test=False):
+        self.test = test
+        filename = "testinput.txt" if self.test else "input.txt"
+        self.compressed = open(filename).read().strip()
+
+    def decode(self, line, part2=False):
+        if not line or line[0] != "(":
+            return 1 + self.decode(line[1:], part2) if line else 0
+        match = re.match(r"\((\d+)x(\d+)\)", line)
+        length, repeat = map(int, match.groups())
+        return repeat * (
+            self.decode(line[match.end() : match.end() + length], part2)
+            if part2
+            else len(line[match.end() : match.end() + length])
+        ) + self.decode(line[match.end() + length :], part2)
+
+    def part1(self):
+        return self.decode(self.compressed)
+
+    def part2(self):
+        return self.decode(self.compressed, part2=True)
+
+
+def main():
+    solution = Solution()
+    print(part1 := f"Part 1: {solution.part1()}")
+    print(part2 := f"Part 2: {solution.part2()}")
+
+    if not solution.test:
+        with open("solution.txt", "w") as f:
+            f.write(f"{part1}\n{part2}\n")
+
+
+if __name__ == "__main__":
+    main()
