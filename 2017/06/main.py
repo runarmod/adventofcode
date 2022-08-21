32a36d683de5c12f3048ddf7e89567d3fdb40578
Runar Saur Modahl <runarmod@gmail.com>
2022-08-21 23:49:34 +0200

    updated 2017 6
    is not written better + faster

--- 2017/06/main.py
+++ 2017/06/main.py
@@ -1,46 +1,31 @@
+from collections import defaultdict
+
+
 class Solution:
     def __init__(self, test=False):
         self.test = test
         filename = "testinput.txt" if self.test else "input.txt"
         self.data = list(map(int, open(filename).read().rstrip().split("\t")))
+        self.run()
+
+    def run(self):
+        self.seen = {}
+        self.counter = 0
+
+        while tuple(self.data) not in self.seen:
+            self.seen[tuple(self.data)] = self.counter
+            idx, val = max(enumerate(self.data), key=lambda x: x[1])
+            self.data[idx] = 0
+
+            for i in range(1, val + 1):
+                self.data[(idx + i) % len(self.data)] += 1
+            self.counter += 1
 
     def part1(self):
-        data = self.data[:]
-        seen = set()
-        while True:
-            seen.add(tuple(data))
-            maxval = max(data)
-            maxidx = data.index(maxval)
-            data[maxidx] = 0
-            for i in range(1, maxval + 1):
-                data[(maxidx + i) % len(data)] += 1
-            if tuple(data) in seen:
-                return len(seen)
+        return self.counter
 
     def part2(self):
-        data = self.data[:]
-        seen = set()
-        find = None
-        while True:
-            seen.add(tuple(data))
-            maxval = max(data)
-            maxidx = data.index(maxval)
-            data[maxidx] = 0
-            for i in range(1, maxval + 1):
-                data[(maxidx + i) % len(data)] += 1
-            if tuple(data) in seen:
-                find = tuple(data)
-                break
-        counter = 0
-        while True:
-            counter += 1
-            maxval = max(data)
-            maxidx = data.index(maxval)
-            data[maxidx] = 0
-            for i in range(1, maxval + 1):
-                data[(maxidx + i) % len(data)] += 1
-            if tuple(data) == find:
-                return counter
+        return self.counter - self.seen[tuple(self.data)]
 
 
 def main():
