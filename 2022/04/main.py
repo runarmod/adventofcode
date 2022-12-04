63fab6e6a105aa26bac5c336dfa32ce6e5dac745
Runar Saur Modahl <runarmod@gmail.com>
2022-12-04 06:12:49 +0100

    2022 4

---
+++ 2022/04/main.py
@@ -0,0 +1,50 @@
+from collections import defaultdict
+import itertools
+import re
+import string
+
+
+def parseLine(line):
+    return tuple(map(int, re.findall(r"(\d+)-(\d+),(\d+)-(\d+)", line)[0]))
+
+
+class Solution:
+    def __init__(self, test=False):
+        self.test = test
+        filename = "testinput.txt" if self.test else "input.txt"
+        self.data = [
+            parseLine(line) for line in open(filename).read().rstrip().split("\n")
+        ]
+
+    def part1(self):
+        return sum(
+            line[0] >= line[2]
+            and line[1] <= line[3]
+            or line[0] <= line[2]
+            and line[1] >= line[3]
+            for line in self.data
+        )
+
+    def part2(self):
+        return sum(
+            bool(
+                set(range(line[0], line[1] + 1)).intersection(
+                    set(range(line[2], line[3] + 1))
+                )
+            )
+            for line in self.data
+        )
+
+
+def main():
+    solution = Solution()
+    print(part1 := f"Part 1: {solution.part1()}")
+    print(part2 := f"Part 2: {solution.part2()}")
+
+    if not solution.test:
+        with open("solution.txt", "w") as f:
+            f.write(f"{part1}\n{part2}\n")
+
+
+if __name__ == "__main__":
+    main()
