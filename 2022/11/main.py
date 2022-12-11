48fcacbea45c48687e8cb59d6d6c6a0787be74f5
Runar Saur Modahl <runarmod@gmail.com>
2022-12-11 19:41:44 +0100

    2022 11

--- 2022/11/main.py
+++ 2022/11/main.py
@@ -1,36 +1,31 @@
-from collections import defaultdict
 from copy import deepcopy
-from pprint import pprint
-import itertools
-import pyperclip
+import math
 import re
-import string
 import functools
-from tqdm import trange
 
 
 def parseMonkey(monkey):
     lines = monkey.split("\n")
     monkey_nr = int(lines[0].split(" ")[1][:-1])
-    starting_items = set(map(int, lines[1].split(": ")[1].split(", ")))
-    operation = lines[2].split(" = ")[1].split("old ")[1]
-    newOp = operation.split(" ")[1]
-    # print(operation)
-    
-    if operation == "* old":
-        operation = lambda x: x ** 2
-    elif operation[0] == "*":
-        operation = lambda x: x * int(newOp)
-    elif operation[0] == "+":
-        operation = lambda x: x + int(newOp)
+    starting_items = list(map(int, lines[1].split(": ")[1].split(", ")))
     test = int(re.findall(r"(\d+)", lines[3])[0])
     true = int(re.findall(r"(\d+)", lines[4])[0])
     false = int(re.findall(r"(\d+)", lines[5])[0])
+
+    function = lines[2].split(" = ")[1].split("old ")[1]
+    newOp = function.split(" ")[1]
+    if function == "* old":
+        function = lambda x: pow(x, 2)
+    elif function[0] == "*":
+        function = lambda x: x * int(newOp)
+    elif function[0] == "+":
+        function = lambda x: x + int(newOp)
+
     return {
         "inspect_count": 0,
         "monkey_nr": monkey_nr,
         "items": starting_items,
-        "operation": operation,
+        "function": function,
         "test": test,
         "true": true,
         "false": false,
@@ -44,87 +39,60 @@ class Solution:
         self.data = [
             parseMonkey(line) for line in open(filename).read().rstrip().split("\n\n")
         ]
-
-    def monkey_round(self, monkey_nr):
-        for item in self.part_1_data[monkey_nr]["items"]:
-            self.part_1_data[monkey_nr]["inspect_count"] += 1
-            op = self.part_1_data[monkey_nr]["operation"]
-            worry = op(item)
-            worry //= 3
-            # print(op)
-            # worry = eval(str(item) + self.part_1_data[monkey_nr]["operation"]) // 3
-            if worry % self.part_1_data[monkey_nr]["test"] == 0:
-                self.part_1_data[self.part_1_data[monkey_nr]["true"]]["items"].add(
-                    worry
-                )
-            else:
-                self.part_1_data[self.part_1_data[monkey_nr]["false"]]["items"].add(
-                    worry
-                )
-        self.part_1_data[monkey_nr]["items"] = set()
+        self.lcm = math.lcm(*[self.data[i]["test"] for i in range(len(self.data))])
 
     def part1(self):
-        self.part_1_data = deepcopy(self.data)
-        for round in range(20):
-            for i, monkey in enumerate(self.data):
-                self.monkey_round(i)
-        return functools.reduce(
-            lambda x, y: x * y,
-            sorted([i["inspect_count"] for i in self.part_1_data], reverse=True)[:2],
-        )
-
-    def monkey_round_2(self, monkey_nr):
-        for item in self.part_2_data[monkey_nr]["items"]:
-            self.part_2_data[monkey_nr]["inspect_count"] += 1
-            op = self.part_2_data[monkey_nr]["operation"]
-            worry = op(item)
-            # worry = eval(str(item) + self.part_2_data[monkey_nr]["operation"])
-            if worry % self.part_2_data[monkey_nr]["test"] == 0:
-                self.part_2_data[self.part_2_data[monkey_nr]["true"]]["items"].add(
-                    worry
-                )
-            else:
-                self.part_2_data[self.part_2_data[monkey_nr]["false"]]["items"].add(
-                    worry
-                )
-        self.part_2_data[monkey_nr]["items"] = set()
+        part_1_data = self.run(deepcopy(self.data), 20)
+        return self.calculate_score(part_1_data)
 
     def part2(self):
-        self.part_2_data = deepcopy(self.data)
-        for round in trange(10000):
-            for i in range(len(self.part_2_data)):
-                self.monkey_round_2(i)
-        pprint(self.part_2_data)
+        part_2_data = self.run(deepcopy(self.data), 10000)
+        return self.calculate_score(part_2_data)
+
+    def monkey_round(self, monkey_nr, data, part1=False):
+        for item in data[monkey_nr]["items"]:
+            self.calculate_worry(monkey_nr, data, part1, item)
+        data[monkey_nr]["items"] = []
+        return data
+
+    def calculate_worry(self, monkey_nr, data, part1, item):
+        data[monkey_nr]["inspect_count"] += 1
+
+        worry = data[monkey_nr]["function"](item)
+        if part1:
+            worry //= 3
+        else:
+            worry %= self.lcm
+
+        true_false = "false" if worry % data[monkey_nr]["test"] else "true"
+        data[data[monkey_nr][true_false]]["items"].append(worry)
+
+    def run(self, data, length):
+        for _ in range(length):
+            for i in range(len(data)):
+                data = self.monkey_round(i, data)
+        return data
+
+    def calculate_score(self, data):
         return functools.reduce(
             lambda x, y: x * y,
-            sorted([i["inspect_count"] for i in self.part_2_data], reverse=True)[:2],
+            sorted([i["inspect_count"] for i in data], reverse=True)[:2],
         )
 
 
 def main():
     test = Solution(test=True)
-    # print(f"(TEST) Part 1: {test.part1()}")
+    print(f"(TEST) Part 1: {test.part1()}")
     print(f"(TEST) Part 2: {test.part2()}")
 
     solution = Solution()
-    # part1 = solution.part1()
+    part1 = solution.part1()
     part2 = solution.part2()
-    # print(part1_text := f"Part 1: {part1}")
+    print(part1_text := f"Part 1: {part1}")
     print(part2_text := f"Part 2: {part2}")
 
-    # copy_answer(part1, part2)
-
-    # with open("solution.txt", "w") as f:
-    #     f.write(f"{part1_text}\n{part2_text}\n")
-
-
-def copy_answer(part1, part2):
-    copy = part1
-    if part2:
-        copy = part2
-
-    if copy:
-        pyperclip.copy(copy)
+    with open("solution.txt", "w") as f:
+        f.write(f"{part1_text}\n{part2_text}\n")
 
 
 if __name__ == "__main__":
