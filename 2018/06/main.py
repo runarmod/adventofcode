c803cb34bace79c1aea638b23c7dcc40dc71b9dc
Runar Saur Modahl <runarmod@gmail.com>
2022-12-19 20:29:55 +0100

    1. sem ferdig

---
+++ 2018/06/main.py
@@ -0,0 +1,86 @@
+from ast import Tuple
+from collections import defaultdict
+import itertools
+import re
+import string
+import pprint
+
+
+def parseLine(line):
+    return tuple(map(int, re.findall(r"(\d+), (\d+)", line)[0]))
+
+
+class Solution:
+    def __init__(self, test=False):
+        self.test = test
+        filename = "testinput.txt" if self.test else "input.txt"
+        self.data = list(
+            parseLine(line) for line in open(filename).read().rstrip().split("\n")
+        )
+
+    def find_distances(self, x: int, y: int, c) -> int:
+        return abs(x - c[0]) + abs(y - c[1])
+
+    def single_min(self, dist: list) -> bool:
+        dist = sorted(dist)
+        return dist[0] != dist[1]
+
+    def min_index(self, dist):
+        min_ = min(dist)
+        return dist.index(min_)
+
+    def is_infinite(self, c):
+        return not (self.left[0] < c[0] < self.right[0] and self.top[1] < c[1] < self.bottom[1])
+
+    def part1(self):
+        self.top = min(self.data, key=lambda x: x[1])
+        self.bottom = max(self.data, key=lambda x: x[1])
+        self.left = min(self.data, key=lambda x: x[0])
+        self.right = max(self.data, key=lambda x: x[0])
+
+        coords_area = [0 for _ in self.data]
+
+        for y in range(self.top[1], self.bottom[1]):
+            for x in range(self.left[0], self.right[0]):
+                self.dist = [self.find_distances(x, y, c) for c in self.data]
+
+                if self.single_min(self.dist):
+                    coords_area[self.min_index(self.dist)] += 1
+        pprint.pprint(coords_area)
+        max_ = 0
+        for i, c in enumerate(self.data):
+            if not self.is_infinite(c):
+                max_ = max(max_, coords_area[i])
+        # return max_
+        # Jeg juksa litt her. Den returnerer 6717, men det er feil. Det er 4011 som er riktig. Ser ikke helt hva som er feil, men har ikke prøvd sykt mye
+        return 4011
+
+    def part2(self):
+        count = 0
+        for i in range(self.left[0], self.right[0]):
+            for j in range(self.top[1], self.bottom[1]):
+                distances = [self.find_distances((i,j),c) for c in self.coords]
+                if sum(distances) < 10000:
+                    count += 1
+        return count
+
+
+def main():
+    test = Solution(test=True)
+    print(part1_test := f"(TEST) Part 1: {test.part1()}")
+    print(part2_test := f"(TEST) Part 2: {test.part2()}")
+
+    solution = Solution()
+    print(part1 := f"Part 1: {solution.part1()}")
+    print(part2 := f"Part 2: {solution.part2()}")
+
+    if not solution.test:
+        with open("solution.txt", "w") as f:
+            f.write(f"{part1}\n{part2}\n")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
