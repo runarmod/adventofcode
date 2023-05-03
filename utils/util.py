e5accb2246b2068e832d5dbaba108d4f54c32352
Runar <40912933+runarmod@users.noreply.github.com>
2023-05-04 00:02:16 +0200

    typing/linting util

--- utils/util.py
+++ utils/util.py
@@ -1,63 +1,78 @@
 import os
 import sys
+from typing import Optional, Union
 
 import pyperclip
 import requests
 from bs4 import BeautifulSoup
-from colorama import Fore, Style
+from colorama import Fore
 from colorama import init as colorama_init
 from dotenv import load_dotenv
-from markdownify import markdownify as md
+from markdownify import markdownify
+
+colorama_init()
 
 load_dotenv()
 COOKIE = os.getenv("COOKIE")
 if COOKIE is None:
     sys.exit(f"{Fore.RED}COOKIE not found in .env")
-colorama_init()
 
 
-def request_submit(year, day, part1, part2):
-    # Send a POST request to the Advent of Code website
-    url = f"https://adventofcode.com/{year}/day/{day}/answer"
+def request_submit(
+    year: Union[int, str], day: Union[int, str], part1: Optional[str], part2: Optional[str]
+) -> None:
+    """
+    Request to submit the answer to the Advent of Code website.
+    This function will decide whether to submit part 1, part 2 or neither.
+    """
+
+    URL = f"https://adventofcode.com/{year}/day/{day}/answer"
 
     if part2:
-        submit(2, part2, url)
+        submit(2, part2, URL)
     elif part1:
-        submit(1, part1, url)
+        submit(1, part1, URL)
     else:
         print(f"{Fore.YELLOW}No answer generated.")
 
 
-def submit(part, answer, url):
-    data = {"level": str(part), "answer": str(answer)}
-    if input(f"Answer for part {part} ({answer}) generated. Send? (y/[n]) ").lower() not in (
-        "y",
-        "ye",
-        "yes",
-    ):
-        print("Did not submit.")
+def submit(part: int, answer: str, url: str) -> None:
+    """
+    Submit the answer to the Advent of Code website, and print the response.
+    """
+    question = f"Answer for part {part} ({answer}) generated. Send? (y/[n]) "
+    if input(question).lower() not in ("y", "ye", "yes"):
+        print(f"{Fore.YELLOW}Did not submit.")
         return
 
+    data = {"level": str(part), "answer": answer}
     response = requests.post(url, data=data, cookies={"session": COOKIE})
+
     if response.status_code != 200:
         print(f"{Fore.RED}Error: {response.status_code}\n{response.content}")
         return
 
     soup = BeautifulSoup(response.content, "html.parser")
     childsoup = soup.find("article")
-    readable_response = md(str(childsoup), heading_style="ATX")
+    readable_response = markdownify(str(childsoup), heading_style="ATX").strip()
     if "That's the right answer!" in readable_response:
         print(f"{Fore.GREEN}Correct!")
     else:
-        print(f"{Fore.RED}{readable_response.strip()}")
+        print(f"{Fore.RED}{readable_response}")
 
 
-def write_solution(path, part1_text, part2_text):
+def write_solution(path: str, part1_text: str, part2_text: str) -> None:
+    """
+    Write the solution to the solution.txt file.
+    """
     with open(os.path.join(path, "solution.txt"), "w") as f:
         f.write(f"{part1_text}\n{part2_text}\n")
 
 
-def copy_answer(part1, part2):
+def copy_answer(part1: Optional[str | int], part2: Optional[str | int]):
+    """
+    Copy the most relevant answer (part 1 or 2) to the clipboard, or neither.
+    """
     copy = part1
     if part2:
         copy = part2
