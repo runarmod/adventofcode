0654daf8aeca0269300214b62a3c46015b317260
Runar <40912933+runarmod@users.noreply.github.com>
2023-05-15 20:49:26 +0200

    2016 DONE!

---
+++ 2016/25/stage4.asm
@@ -0,0 +1,25 @@
+d=a
+c=7
+d+=365*c
+b=0
+c=0
+while 1:
+    a=d
+    while a != 0:
+        b=a
+        a=0
+        while 1:
+            c=2
+            while c != 0:
+                if b == 0:
+                    break twice
+                b--
+                c--
+            a++
+        b=2
+        while 1:
+            if c == 0:
+                break
+            b--
+            c--
+        print(b)
