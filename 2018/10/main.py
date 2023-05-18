13e3b5840195a6f6d8dce94b0da143f82d38117b
Runar <40912933+runarmod@users.noreply.github.com>
2023-05-18 18:12:27 +0200

    optimized 2018 10

--- 2018/10/main.py
+++ 2018/10/main.py
@@ -1,44 +1,27 @@
-import itertools
 import re
 import time
 
 
 def parseLine(line):
-    return Marble(*map(int, re.findall(r"-?\d+", line)))
-
-
-class Marble:
-    def __init__(self, x, y, dx, dy):
-        self.x = x
-        self.y = y
-        self.original_x = x
-        self.original_y = y
-        self.dx = dx
-        self.dy = dy
-
-    def step(self):
-        self.x += self.dx
-        self.y += self.dy
-
-    def get_coord(self):
-        return self.x, self.y
+    return tuple(map(int, re.findall(r"-?\d+", line)))
 
 
 class Solution:
     def __init__(self, test=False):
         self.test = test
         filename = "testinput.txt" if self.test else "input.txt"
-        self.marbles = {parseLine(line) for line in open(filename).read().rstrip().split("\n")}
+        self.marbles = tuple(map(parseLine, open(filename).read().rstrip().split("\n")))
         self.run()
 
-    def step(self):
-        for marble in self.marbles:
-            marble.step()
-
-    def area(self):
-        return (
-            max(marble.x for marble in self.marbles) - min(marble.x for marble in self.marbles)
-        ) * (max(marble.y for marble in self.marbles) - min(marble.y for marble in self.marbles))
+    def area(self, marbles):
+        max_x = max_y = -float("inf")
+        min_x = min_y = float("inf")
+        for marble in marbles:
+            max_x = max(max_x, marble[0])
+            max_y = max(max_y, marble[1])
+            min_x = min(min_x, marble[0])
+            min_y = min(min_y, marble[1])
+        return (max_x - min_x) * (max_y - min_y)
 
     def show(self, lst):
         coords = lst
@@ -54,22 +37,44 @@ class Solution:
                     print(" ", end="")
             print()
 
-    def run(self):
-        record = float("inf")
-        prev = [marble.get_coord() for marble in self.marbles]
+    def get_area_marbles(self, time):
+        marbles = tuple(
+            (marble[0] + marble[2] * time, marble[1] + marble[3] * time) for marble in self.marbles
+        )
+        area = self.area(marbles)
+        return area, marbles
 
-        for i in itertools.count():
-            self.step()
-            area = self.area()
-            record = min(record, area)
-            if area > record:
-                self.result1 = "HI" if self.test else "FNRGPBHR"
-                self.result2 = i
-                self.show(prev)
-                break
-            prev = [marble.get_coord() for marble in self.marbles]
+    def find_lowest_area(self, low, high):
+        if high < low:
+            return self.get_area_marbles(0)[1], 0
+
+        if high == low:
+            return self.get_area_marbles(low)[1], low
+
+        mid = (low + high) // 2
+
+        # Mid is lowest point
+        if (
+            self.get_area_marbles(mid - 1)[0]
+            > self.get_area_marbles(mid)[0]
+            < self.get_area_marbles(mid + 1)[0]
+        ):
+            return self.get_area_marbles(mid)[1], mid
+
+        # Gets lower to the right
+        if self.get_area_marbles(mid - 1)[0] > self.get_area_marbles(mid)[0]:
+            return self.find_lowest_area(mid + 1, high)
+
+        # Gets lower to the left
+        if self.get_area_marbles(mid)[0] < self.get_area_marbles(mid + 1)[0]:
+            return self.find_lowest_area(low, mid - 1)
+
+    def run(self):
+        self.winning_coords, self.result2 = self.find_lowest_area(0, 20_000)
 
     def part1(self):
+        self.show(self.winning_coords)
+        self.result1 = "HI" if self.test else "FNRGPBHR"
         return self.result1
 
     def part2(self):
