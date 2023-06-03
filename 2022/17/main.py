4e2612e5f0fc3d9dc24c32daa94d3b01f1b12dac
Runar <40912933+runarmod@users.noreply.github.com>
2023-06-04 01:03:26 +0200

    2022 17

--- 2022/17/main.py
+++ 2022/17/main.py
@@ -1,156 +1,107 @@
-from collections import defaultdict
-from pprint import pprint
+from operator import lshift, rshift
 import itertools
-import pyperclip
-import re
-import string
+import time
 
-from tqdm import trange
+
+class Rock:
+    def __init__(self, figure: list[int], i: int, position: int = 0b0010000):
+        self.figure = figure
+        self.index = i
+        self.position = position
+        self.height = len(figure)
+
+    def shift(self, direction):
+        operation = {">": rshift, "<": lshift}[direction]
+        return Rock(
+            [operation(row, 1) for row in self.figure], self.index, operation(self.position, 1)
+        )
+
+    def collision(self, grid):
+        return any(my_row & other_row for my_row, other_row in zip(self.figure, grid))
+
+    def will_crash_wall(self, direction):
+        wall = {">": 0b1, "<": 0b1000000}[direction]
+        return any(row & wall for row in self.figure)
 
 
 class Solution:
     def __init__(self, test=False):
-        self.test = test
-        filename = "testinput.txt" if self.test else "input.txt"
-        self.instructions = open(filename).read().rstrip()
-        self.shapes = [
-            "####".split("\n"),
-            ".#.\n###\n.#.".split("\n"),
-            "..#\n..#\n###".split("\n"),
-            "#\n#\n#\n#".split("\n"),
-            "##\n##".split("\n"),
+        filename = "testinput.txt" if test else "input.txt"
+        self.instructions = list(enumerate(open(filename).read().rstrip()))
+        self.SHAPES = [
+            Rock([0b0011110], 0),
+            Rock([0b0001000, 0b0011100, 0b0001000], 1),
+            Rock([0b0000100, 0b0000100, 0b0011100], 2),
+            Rock([0b0010000, 0b0010000, 0b0010000, 0b0010000], 3),
+            Rock([0b0011000, 0b0011000], 4),
         ]
 
-    def move_left(self, shape, l, fall_distance):
-        for i in range(len(shape)):
-            for j in range(len(shape[0])):
-                if shape[i][j] == "#":
-                    if j == 0 or shape[i][j - 1] != "#":
-                        if self.diagram[i + fall_distance][j + l - 1] == "#":
-                            return False
-        for i in range(len(shape)):
-            for j in range(len(shape[0])):
-                if shape[i][j] == "#":
-                    self.diagram[i + fall_distance][j + l - 1] = "#"
-                    self.diagram[i + fall_distance][j + l] = "."
-        return True
+    def run(self, iterations):
+        instructions, shapes = itertools.cycle(self.instructions), itertools.cycle(self.SHAPES)
+        grid = [0b0] * 50_000
+        top = len(grid)
+        height = 0
 
-    def move_right(self, shape, l, fall_distance):
-        for i in range(len(shape)):
-            for j in range(len(shape[0]) - 1, -1, -1):
-                if shape[i][j] == "#":
-                    if j == len(shape[i]) - 1 or shape[i][j + 1] != "#":
-                        if self.diagram[i + fall_distance][j + l + 1] == "#":
-                            return False
-        for i in range(len(shape)):
-            for j in range(len(shape[0]) - 1, -1, -1):
-                if shape[i][j] == "#":
-                    self.diagram[i + fall_distance][j + l + 1] = "#"
-                    self.diagram[i + fall_distance][j + l] = "."
-        return True
+        visited: dict[
+            tuple[int, int, int], tuple[int, int]
+        ] = {}  # (rock_index, instruction, position) -> (height, iteration)
 
-    def move_down(self, shape, l, fall_distance):
-        for i in range(len(shape) - 1, -1, -1):
-            for j in range(len(shape[i])):
-                if shape[i][j] == "#":
-                    if i == len(shape) - 1 or shape[i + 1][j] != "#":
-                        if self.diagram[i + fall_distance + 1][j + l] == "#":
-                            return False
-        for i in range(len(shape) - 1, -1, -1):
-            for j in range(len(shape[i])):
-                if shape[i][j] == "#":
-                    self.diagram[i + fall_distance + 1][j + l] = "#"
-                    self.diagram[i + fall_distance][j + l] = "."
-        return True
+        instruction = (-1, ">")  # DUMMY
 
-    def extend_diagram(self):
-        for i in range(len(self.diagram)):
-            if self.diagram[i].count("#"):
-                break
-        if i < 2:
-            while i < 2:
-                self.diagram = [["." for _ in range(7)]] + self.diagram
-                i += 1
-        elif i > 3:
-            while i > 3:
-                self.diagram = self.diagram[1:]
-                i -= 1
+        for rock_nr in itertools.count():
+            rock = next(shapes)
+            y = top - rock.height - 3
 
-    def add_shape(self, shape):
-        new_shape = []
-        for i in range(len(shape)):
-            new_shape.append(
-                ["." for _ in range(2)]
-                + list(shape[i])
-                + ["." for _ in range(7 - len(shape[i]) - 2)]
-            )
+            for y in itertools.count(y):
+                instruction = next(instructions)
 
-        self.diagram = new_shape + self.diagram
+                if not rock.will_crash_wall(instruction[1]):
+                    shifted_rock = rock.shift(instruction[1])
+                    if not shifted_rock.collision(grid[y:]):
+                        rock = shifted_rock
+
+                if rock.collision(grid[y + 1 :]) or rock.height + y >= len(grid):
+                    for i in range(rock.height):
+                        grid[y + i] |= rock.figure[i]
+                    break
+
+            top = min(top, y)
+            height = len(grid) - top
+
+            current_state = (rock.index, instruction[0], rock.position)
+
+            if current_state not in visited:
+                visited[current_state] = (height, rock_nr)
+                continue
+
+            cycle_size = rock_nr - visited[current_state][1]
+            remaining_iterations = iterations - rock_nr - 1
+            remaining, remainder = divmod(remaining_iterations, cycle_size)
+            if remainder == 0:  # Perfect ending
+                cycle_height = height - visited[current_state][0]
+                return int(height + cycle_height * remaining)
+
+        return height
 
     def part1(self):
-        return self.run(1)
-
-    def run(self, part):
-        self.diagram = [["." for _ in range(7)] for _ in range(3)] + [
-            ["#" for _ in range(7)]
-        ]
-        instruction_index = -1
-        shape_index = -1
-
-        for num in trange(2022 if part == 1 else 1000000000000):
-            if num % 10 == 0:
-                self.diagram = self.diagram[:20]
-            shape_index = (shape_index + 1) % len(self.shapes)
-            shape = self.shapes[shape_index]
-            l = 2
-            # r = len(shape[0]) + l - 1
-            fall_distance = -1
-            self.extend_diagram()
-            self.add_shape(self.shapes[shape_index])
-            while True:
-                fall_distance += 1
-                instruction_index = (instruction_index + 1) % len(self.instructions)
-                direction = self.instructions[instruction_index]
-                if direction == "<":
-                    if l > 0:
-                        l -= self.move_left(shape, l, fall_distance)
-                elif direction == ">":
-                    if l + len(shape[0]) - 1 < 6:
-                        l += self.move_right(shape, l, fall_distance)
-                if not self.move_down(shape, l, fall_distance):
-                    break
-        # pprint(self.diagram)
-        self.extend_diagram()
-        return len(self.diagram) - 1 - 3
+        return self.run(2022)
 
     def part2(self):
-        return self.run(2)
+        return self.run(1e12)
 
 
 def main():
     test = Solution(test=True)
     print(f"(TEST) Part 1: {test.part1()}")
     print(f"(TEST) Part 2: {test.part2()}")
-    
+
+    start = time.perf_counter()
+
     solution = Solution()
-    part1 = solution.part1()
-    part2 = solution.part2()
-    print(part1_text := f"Part 1: {part1}")
-    print(part2_text := f"Part 2: {part2}")
+    print(f"Part 1: {solution.part1()}")
+    print(f"Part 2: {solution.part2()}")
 
-    copy_answer(part1, part2)
-
-    with open("solution.txt", "w") as f:
-        f.write(f"{part1_text}\n{part2_text}\n")
-
-
-def copy_answer(part1, part2):
-    copy = part1
-    if part2:
-        copy = part2
-
-    if copy:
-        pyperclip.copy(copy)
+    print(f"\nTotal time: {time.perf_counter() - start : .4f} sec")
 
 
 if __name__ == "__main__":
