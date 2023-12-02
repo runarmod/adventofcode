ee72368cec7d8202d4790b7b58c9c4195befcd2a
Runar Saur Modahl <runarmod@gmail.com>
2023-12-02 07:20:57 +0100

    2023 02 rework

--- 2023/02/main.py
+++ 2023/02/main.py
@@ -1,57 +1,34 @@
+import re
 import time
-from collections import defaultdict
-
-
-def parseLine(line):
-    line_data = []
-    splitted = line.split(" ")
-    line_data.append(int(splitted[1][:-1]))
-    subsection = []
-    for i in range(2, len(splitted), 2):
-        if splitted[i + 1][-1] == ";":
-            subsection.append((int(splitted[i]), splitted[i + 1][:-1]))
-            line_data.append(subsection)
-            subsection = []
-        elif splitted[i + 1][-1] == ",":
-            subsection.append((int(splitted[i]), splitted[i + 1][:-1]))
-        else:
-            subsection.append((int(splitted[i]), splitted[i + 1]))
-    if subsection:
-        line_data.append(subsection)
-    return line_data
 
 
 class Solution:
     def __init__(self, test=False):
         self.test = test
         filename = "testinput.txt" if self.test else "input.txt"
-        self.data = [
-            parseLine(line) for line in open(filename).read().rstrip().split("\n")
+        data = [
+            re.findall(r"(\d+) (\w+)", line)
+            for line in open(filename).read().rstrip().split("\n")
+        ]
+        self.max_per_line = [
+            {
+                color: max(int(pair[0]) for pair in line if pair[1] == color)
+                for color in ("green", "red", "blue")
+            }
+            for line in data
         ]
 
     def part1(self):
-        return sum(line[0] for line in self.data if self.possible(line))
-
-    def possible(self, line):
-        for instance in line[1:]:
-            green = sum(a[0] for a in instance if a[1] == "green")
-            red = sum(a[0] for a in instance if a[1] == "red")
-            blue = sum(a[0] for a in instance if a[1] == "blue")
-            if red > 12 or green > 13 or blue > 14:
-                return False
-        return True
+        return sum(
+            game_nr
+            for game_nr, game in enumerate(self.max_per_line, start=1)
+            if not (game["red"] > 12 or game["green"] > 13 or game["blue"] > 14)
+        )
 
     def part2(self):
-        sum_power_of_sets = 0
-        for line in self.data:
-            game = defaultdict(int)
-            for instance in line[1:]:
-                for color in ("green", "red", "blue"):
-                    game[color] = max(
-                        game[color], sum(a[0] for a in instance if a[1] == color)
-                    )
-            sum_power_of_sets += game["green"] * game["red"] * game["blue"]
-        return sum_power_of_sets
+        return sum(
+            game["red"] * game["green"] * game["blue"] for game in self.max_per_line
+        )
 
 
 def main():
