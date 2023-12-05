226074e50b87f25d72b16c0e4ea8f96f9f71fe66
Runar Saur Modahl <runarmod@gmail.com>
2023-12-05 14:34:58 +0100

    Remove task download
    In FAQ: "please don't include parts of Advent of Code like the puzzle text"
    
    (Should probably not include input either, but its really good to have :/ )

--- setup.py
+++ setup.py
@@ -10,11 +10,9 @@ from datetime import datetime
 
 import pytz
 import requests
-from bs4 import BeautifulSoup
 from colorama import Fore
 from colorama import init as colorama_init
 from dotenv import load_dotenv
-from markdownify import markdownify as md
 
 from utils import countdown
 
@@ -155,23 +153,6 @@ def main():
         print(f"{Fore.GREEN}Input downloaded to {inputPath}")
     else:
         print(f"{Fore.YELLOW}Inputfile already exists. Continuing...")
-
-    if not os.path.exists(taskPath := os.path.join(path, "task.md")) or args.f:
-        cookies = {"session": COOKIE}
-        taskURL = f"{URL}"
-        headers = {"User-Agent": USER_AGENT}
-        page = requests.get(taskURL, cookies=cookies, headers=headers)
-
-        if page.status_code != 200:
-            sys.exit(
-                f"{Fore.RED}Task download failed\nError: {page.status_code}\n{page.content}"
-            )
-
-        soup = BeautifulSoup(page.content, "html.parser")
-        childsoup = soup.find("article")
-        with open(taskPath, "w") as f:
-            f.write(md(str(childsoup), heading_style="ATX"))
-        print(f"{Fore.GREEN}Task downloaded to {taskPath}")
 
     # Open browser
     if args.b:
