92e781da66403f351559ba83ccda016b1235da3b
Runar Saur Modahl <runarsm@stud.ntnu.no>
2024-03-23 18:24:45 +0100

    2019 21

--- 2019/21/main.py
+++ 2019/21/main.py
@@ -1,4 +1,3 @@
-import os
 import time
 
 from IntcodeComputer import IntcodeComputer
@@ -7,25 +6,36 @@ from IntcodeComputer import IntcodeComputer
 class Solution:
     def __init__(self):
         self.data = list(map(int, open("input.txt").read().rstrip().split(",")))
-        self.computer = IntcodeComputer(self.data)
 
     def part1(self):
-        program = """NOT A J
-NOT B T
-OR T J
-NOT C T
-OR T J
-AND D J
-WALK"""
-        self.computer.inputs = list(map(ord, program + "\n"))
-        for a in self.computer.iter():
-            try:
-                print(chr(a), end="")
-            except ValueError:
-                return int(a)
-        return None
+        # !(A && B && C) && D
+        program = ("OR A J", "AND B J", "AND C J", "NOT J J", "AND D J", "WALK")
+        return self.run_program(program)
 
     def part2(self):
+        # !(A && B && C) && D && (E || H)
+        program = (
+            "OR A J",
+            "AND B J",
+            "AND C J",
+            "NOT J J",
+            "AND D J",
+            "OR E T",
+            "OR H T",
+            "AND T J",
+            "RUN",
+        )
+        return self.run_program(program)
+
+    def run_program(self, program):
+        computer = IntcodeComputer(self.data)
+        computer.inputs = list(map(ord, "\n".join(program) + "\n"))
+        for output_char in computer.iter():
+            try:
+                # print(chr(output_char), end="")
+                chr(output_char)
+            except ValueError:
+                return int(output_char)
         return None
 
 
@@ -37,6 +47,7 @@ def main():
     print(f"Part 2: {solution.part2()}")
 
     print(f"\nTotal time: {time.perf_counter() - start : .4f} sec")
+
 
 if __name__ == "__main__":
     main()
