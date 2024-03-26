c596853663b88f04be427d649c2add83c4641257
Runar Saur Modahl <runarsm@stud.ntnu.no>
2024-03-27 00:29:49 +0100

    2019 16

--- 2019/16/main.py
+++ 2019/16/main.py
@@ -1,22 +1,12 @@
-import functools
 import itertools
-import os
-import re
-import string
-import sys
 import time
-from collections import defaultdict, deque
-from pprint import pprint
-
-sys.path.insert(0, "../../")
-from utils import copy_answer, request_submit, write_solution
 
 
 class Solution:
     def __init__(self, test=False):
         self.test = test
         filename = "testinput.txt" if self.test else "input.txt"
-        self.data = [int(x) for x in open(filename).read().rstrip()]
+        self.data = list(map(int, open(filename).read().rstrip()))
         self.pattern = [0, 1, 0, -1]
 
     def get_pattern(self, idx):
@@ -43,7 +33,12 @@ class Solution:
         return int("".join(map(str, nums[:8])))
 
     def part2(self):
-        return None
+        nums = self.data * 10_000
+        offset = int("".join(map(str, nums[:7])))
+        for _ in range(100):
+            for i in range(len(nums) - 1, offset, -1):
+                nums[i - 1] = (nums[i - 1] + nums[i]) % 10
+        return int("".join(map(str, nums[offset : offset + 8])))
 
 
 def main():
@@ -53,21 +48,17 @@ def main():
     test1 = test.part1()
     test2 = test.part2()
     print(
-        f"(TEST) Part 1: {test1}, \t{'correct :)' if test1 == 52432133 else 'wrong :('}"
+        f"(TEST) Part 1: {test1}, \t{'correct :)' if test1 == 24465799 else 'wrong :('}"
     )
-    print(f"(TEST) Part 2: {test2}, \t{'correct :)' if test2 == None else 'wrong :('}")
+    print(
+        f"(TEST) Part 2: {test2}, \t{'correct :)' if test2 == 84462026 else 'wrong :('}"
+    )
 
     solution = Solution()
-    part1 = solution.part1()
-    part2 = solution.part2()
-    print(part1_text := f"Part 1: {part1}")
-    print(part2_text := f"Part 2: {part2}")
+    print(f"Part 1: {solution.part1()}")
+    print(f"Part 2: {solution.part2()}")
 
     print(f"\nTotal time: {time.perf_counter() - start : .4f} sec")
-
-    copy_answer(part1, part2)
-    write_solution(os.path.dirname(os.path.realpath(__file__)), part1_text, part2_text)
-    request_submit(2019, 16, part1, part2)
 
 
 if __name__ == "__main__":
