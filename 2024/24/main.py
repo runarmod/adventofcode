748947df14e5fbd023b9d2654664731cc3006c06
Runar Saur Modahl <runarmod@gmail.com>
2024-12-28 16:18:10 +0100

    2024 24 generalized

--- 2024/24/main.py
+++ 2024/24/main.py
@@ -1,5 +1,8 @@
 import itertools
+import random
 import time
+from collections import deque
+from copy import deepcopy
 
 from aoc_utils_runarmod import get_data
 
@@ -17,100 +20,107 @@ class Solution:
             self.wires[wire] = int(val)
 
         self.gates = {v: k for k, v in self.last}
+        self.max_z = max(
+            map(lambda x: int(x[1:]), filter(lambda x: x.startswith("z"), self.gates))
+        )
 
-    def eval(self, out):
-        if out in self.wires:
-            return self.wires[out]
-        a, op, b = self.gates[out].split()
+    def calculate(self, x: int, y: int, gates: dict[str, str]) -> int:
+        wires = {}
+        assert x.bit_length() <= self.max_z, f"{x.bit_length()=}"
+        assert y.bit_length() <= self.max_z, f"{y.bit_length()=}"
+        for i in range(self.max_z):
+            wires[f"x{i:02}"] = (x >> i) & 1
+            wires[f"y{i:02}"] = (y >> i) & 1
+
+        return sum(self.eval(f"z{i:02}", wires, gates) << i for i in range(self.max_z))
+
+    def eval(self, wire, wires=None, gates=None):
+        if gates is None:
+            gates = self.gates
+        if wires is None:
+            wires = self.wires
+
+        if wire in wires:
+            return wires[wire]
+        a, op, b = gates[wire].split()
+        a_val = self.eval(a, wires, gates)
+        b_val = self.eval(b, wires, gates)
         match op:
             case "AND":
-                v = self.eval(a) & self.eval(b)
+                v = a_val & b_val
             case "OR":
-                v = self.eval(a) | self.eval(b)
+                v = a_val | b_val
             case "XOR":
-                v = self.eval(a) ^ self.eval(b)
-        self.wires[out] = v
+                v = a_val ^ b_val
+        wires[wire] = v
         return v
 
     def part1(self):
-        out = []
-        for i in itertools.count():
-            name = "z" + str(i).zfill(2)
-            if name not in self.gates:
-                break
-            out.append(self.eval(name))
-        self.z = int("".join(map(str, out[::-1])), 2)
-        return self.z
+        return sum(self.eval(f"z{i:02}") << i for i in range(self.max_z + 1))
 
-    def visualize(self, mistakes):
-        with open("graph.dot", "w") as f:
-            f.write("digraph G {\n")
-            for k, v in self.gates.items():
-                a, op, b = v.split()
-                f.write(f'"{a}" -> "{a}{op}{b}"\n')
-                f.write(f'"{b}" -> "{a}{op}{b}"\n')
-                f.write(f'"{a}{op}{b}" -> "{k}"\n')
-                if k in mistakes:
-                    f.write(f'"{k}" [color=red, style=filled, fillcolor=lightpink];\n')
-            f.write("}")
+    def first_mistake(self, gates: dict[str, str]) -> int:
+        first = float("inf")
+        for _ in range(self.max_z):  # TODO: find a better way to find the first mistake
+            # (There is no logic in looping exactly self.max_z times,
+            #  other than as we have larger numbers, we should try more numbers)
+            x, y = random.getrandbits(self.max_z), random.getrandbits(self.max_z)
+            xy = x + y
+            z = self.calculate(x, y, gates)
+            if z == xy:
+                continue
+            # https://stackoverflow.com/a/40354822/10880273
+            pos = ((xy ^ z) & (-(xy ^ z))).bit_length() - 1
+            if pos != -1 and pos < first:
+                first = pos
+        return first
+
+    def get_ancestors(
+        self,
+        wire: str,
+        gates: dict[str, str],
+        include_self: bool = True,
+        include_inputs: bool = False,
+    ):
+        if include_self:
+            yield wire
+        if wire[0] in "xy":
+            return
+        a, _, b = gates[wire].split()
+        q = deque([a, b])
+        while q:
+            node = q.popleft()
+            if not include_inputs and node[0] in "xy":
+                continue
+            yield node
+            if node[0] not in "xy":
+                a, _, b = gates[node].split()
+                q.extend([a, b])
+
+    def find_swap_pair(self):
+        worst_z_v = self.first_mistake(self.gates)
+        for swap1, swap2 in itertools.product(
+            self.get_ancestors(f"z{worst_z_v:02}", self.gates), self.gates
+        ):
+            if swap1 == swap2:
+                continue
+            gates = deepcopy(self.gates)
+            gates[swap1], gates[swap2] = gates[swap2], gates[swap1]
+            try:
+                new_worst = self.first_mistake(gates)
+            except RecursionError:
+                continue
+            if new_worst <= worst_z_v:  # Bad swap
+                continue
+            return swap1, swap2
+        else:
+            assert False, "No swaps found. Try again."
 
     def part2(self):
-        def match_pattern(node, pattern):
-            if pattern == "*" or node not in self.gates:
-                return set()
-            a, op, b = self.gates[node].split()
-            if pattern[0] != op:
-                return {node}
-
-            e_ab = match_pattern(a, pattern[1]) | match_pattern(b, pattern[2])
-            e_ba = match_pattern(b, pattern[1]) | match_pattern(a, pattern[2])
-            return min(
-                e_ab, e_ba, key=len
-            )  # THe minimum set of nodes that contribute to the mistake
-
-        mistakes = set()
-        for n in itertools.count(start=1):
-            z = f"z{n:02}"
-            if z not in self.gates:
-                break
-            prev = n - 1
-            corresponding_x = f"x{n:02}"
-            corresponding_y = f"y{n:02}"
-
-            prev_x = f"x{prev:02}"
-            prev_y = f"y{prev:02}"
-
-            mistakes |= match_pattern(
-                z,
-                (
-                    "XOR",
-                    ("XOR", corresponding_x, corresponding_y),
-                    (
-                        "OR",
-                        ("AND", prev_x, prev_y),
-                        ("AND", ("XOR", prev_x, prev_y), "*"),
-                    ),
-                ),
-            )
-
-        # self.visualize(mistakes)
-        print(mistakes)
-
-        known_false_positive = {"z45", "drq"}  # All the way in the beginning and end
-        """
-        By observation of the dot graph, we can see that the only way to make z16 correct,
-        is if rpb gets its input from an AND gate that's connected to x15 and y15,
-        and for ctg to get its input from an XOR gate (from x15 and y15), such that z15
-        will get its correct output, and such that the output of ctg is correctly sent down
-        to z16 (and onwards). Therefore, ctg and rpb need to be swapped, and vsd and wmb
-        are false positives. (See graph.png)
-        """
-        visual_false_positive = {"wmb", "vsd"}
-        visual_false_negative = {"rpb"}
-        swaps = (
-            mistakes - known_false_positive - visual_false_positive
-            | visual_false_negative
-        )
+        swaps = []
+        for _ in range(4):
+            swap1, swap2 = self.find_swap_pair()
+            swaps.extend((swap1, swap2))
+            self.gates[swap1], self.gates[swap2] = self.gates[swap2], self.gates[swap1]
         return ",".join(sorted(swaps))
 
 
