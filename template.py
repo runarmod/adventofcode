7642115089174b059164127b46b2aecb3e7e21c4
Runar Saur Modahl <runarmod@gmail.com>
2025-11-30 23:18:17 +0100

    use pre-commit

--- template.py
+++ template.py
@@ -79,7 +79,7 @@ def main():
     print(part1_text := f"Part 1: {part1}")
     print(part2_text := f"Part 2: {part2}")
 
-    print(f"\nTotal time: {time.perf_counter() - start : .4f} sec")
+    print(f"\nTotal time: {time.perf_counter() - start: .4f} sec")
 
     copy_answer(part1, part2)
     write_solution(os.path.dirname(os.path.realpath(__file__)), part1_text, part2_text)
